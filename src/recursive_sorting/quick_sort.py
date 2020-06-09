65b4103ece7bcd1f06b2c37bb39f7eae6df2cc6c
Cesare Ferrari <cesare@maryandferrari.com>
2020-06-09 14:01:59 -0400

    Add factorial'

--- src/recursive_sorting/quick_sort.py
+++ src/recursive_sorting/quick_sort.py
@@ -5,10 +5,10 @@ def partition(arr):
 
     # iterate over the rest of the array 
     for num in arr[1:]:
-    # if the element is greater than pivot, in the right
+        # if the element is greater than pivot, in the right
         if num > pivot:
             right.append(num)
-    # else, in the left
+        # else, in the left
         else:
             left.append(num)
 
@@ -22,9 +22,14 @@ def quicksort(arr):
     # partition here into left, pivot, and right
     left, pivot, right = partition(arr)
 
-    return quicksort(left) + pivot + quicksort(right)
-
+    return quicksort(left) + [pivot] + quicksort(right)
 
 
 def quicksort_in_place(arr):
     pass
+
+
+
+items = [3, 6, 1, 5, 4, 2]
+result = quicksort(items)
+print(result)
