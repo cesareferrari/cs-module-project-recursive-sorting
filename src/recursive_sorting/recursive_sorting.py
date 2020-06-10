8b243815879b1f1d2e8a369959d347439c016285
Cesare Ferrari <cesare@maryandferrari.com>
2020-06-09 21:18:23 -0400

    Add merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,20 +1,54 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    # elements = len(arrA) + len(arrB)
+    # array that holds the sorted elements from A and B
+    # merged_arr = [0] * elements
+    merged_arr = []
 
-    # Your code here
+    # initialize the pointers that start at each array
+    a = 0
+    b = 0
 
+    while a < len(arrA) and b < len(arrB):
+        # compare the elements where a and b point to
+        # and append the correct element from array A or B to merged_arr
+        if arrA[a] < arrB[b]:
+            merged_arr.append(arrA[a])
+            a += 1
+        else:
+            merged_arr.append(arrB[b])
+            b += 1
+
+    # one of the lists may have extra elements:
+    # append all of them to merged_arr
+    # only one of these loops is going to run
+    while a < len(arrA):
+        merged_arr.append(arrA[a])
+        a += 1
+
+    while b < len(arrB):
+        merged_arr.append(arrB[b])
+        b += 1
 
     return merged_arr
 
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-    # Your code here
+    # break the array down recursively
+    # base case: when list is length of 1
+    if len(arr) > 1:
+        # if length more than 1: keep recursing
+        # break array down into left and right subarrays
+        # split into 2 subarrays
+        # left half contains elements from 0 to half
+        left = merge_sort(arr[:len(arr) // 2])
+        # right half contains elements from half to end
+        right = merge_sort(arr[len(arr) // 2:])
 
+        # merge them back up
+        arr = merge(left, right)
 
     return arr
+
 
 
 # implement an in-place merge sort algorithm
